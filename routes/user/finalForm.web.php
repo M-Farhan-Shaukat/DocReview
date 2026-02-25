cac5937294056283dde12b37bfa3dc7cc552bb22
M.Farhan <100473373+M-Farhan-Shaukat@users.noreply.github.com>
2026-02-25 13:20:08 +0500

    fix

--- routes/user/finalForm.web.php
+++ routes/user/finalForm.web.php
@@ -3,27 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\User\FinalFormController;
 
-Route::get('/final-form/review',[FinalFormController::class, 'review'])->name('final.review');
-
-//Route::post('/final-form/submit',[FinalFormController::class, 'submit'])->name('final.submit');
-Route::get('/user/final-form/preview',
-    [FinalFormController::class, 'formPreview'])
-    ->name('final.preview');
-
 Route::get('/final_forms', [FinalFormController::class, 'index'])->name('final_form.index');
-Route::get('/final_form/{id}', [FinalFormController::class, 'show'])->name('final_form.view');
-
-Route::post('final_form/{id}/upload', [FinalFormController::class, 'uploadDocuments'])->name('final_form.upload');
-
-
-
-
 Route::get('/final-form', [FinalFormController::class, 'create'])->name('final_form.create');
-Route::post('/final-form/preview', [FinalFormController::class, 'preview'])
-    ->name('final-form.preview');
-
-Route::post('/final-form/store', [FinalFormController::class, 'store'])
-    ->name('final-form.store');
-
-Route::get('/user/form/download/{application}', [FinalFormController::class, 'downloadPdf'])
-    ->name('form.download');
+Route::post('/final-form/store', [FinalFormController::class, 'store'])->name('final-form.store');
+Route::get('/final_form/{id}', [FinalFormController::class, 'show'])->name('final_form.view');
+Route::get('/user/form/download/{application}', [FinalFormController::class, 'downloadPdf'])->name('form.download');
